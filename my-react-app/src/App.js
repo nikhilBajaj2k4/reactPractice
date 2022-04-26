5fd3650c5e3d12c9adf62d1cad964dfed8dc19b2
nick399 <74670912+nick399@users.noreply.github.com>
2022-04-27 04:13:33 +0530

    idk tf i do

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,26 +1,19 @@
 import './App.css';
+import Header from './Components/Header'
 import { useState } from "react"
 
 function App() {
 
-  const [names, setNames] = useState([{
-    name: 'Gilfoyl',
-    session: true,
-    gamestag: 44,
-  }])
+  const namings = ["Nikhil", "Amit", "Jesus"]
 
-const session = names[0].session
-
-
-  function handleClick(){
-    setNames(prevArray => [{...prevArray, session: !session}])
-    console.log("changed")
-  }
+  const newNames = namings.map((name) => {
+    return <h2>{name}</h2>
+  })
 
   return (
     <div className="App">
-        <button onClick={handleClick}>{!session ? "eye" : "eyeHide"}</button>
-        {session && <img className="er" src='https://assets3.thrillist.com/v1/image/2845547/1584x1056/scale;webp=auto;jpeg_quality=60;progressive.jpg'></img>}
+      <Header check='Heya' />
+      <p>{newNames}</p>
     </div>
   );
 }
